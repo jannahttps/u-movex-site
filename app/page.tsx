72734dea651d340a6b06da022088053d193595a3
Janna <wwwjeanneri@gmail.com>
2026-04-07 01:24:52 -0400

    Import SiteHeader component in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import SiteHeader from './components/SiteHeader';
 import HeroSection from './components/HeroSection';
 import ServicesSection from './components/ServicesSection';
 import WhyChooseUsSection from './components/WhyChooseUsSection';
@@ -9,6 +10,7 @@ import Footer from './components/Footer';
 export default function Home() {
   return (
     <main className="min-h-screen">
+      <SiteHeader />
       <HeroSection />
       <ServicesSection />
       <WhyChooseUsSection />
