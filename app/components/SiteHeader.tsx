0966d2dc77b8883c1154488558f08f016cd6083c
Janna <wwwjeanneri@gmail.com>
2026-04-07 17:41:55 -0400

    Update logo image size in SiteHeader component

--- app/components/SiteHeader.tsx
+++ app/components/SiteHeader.tsx
@@ -49,10 +49,10 @@ export default function SiteHeader({ dark = false }: SiteHeaderProps) {
             onClick={() => setOpen(false)}
           >
             <img
-              src="/logo-clean.png"
-              alt="U-MOVEX Orlando Movers"
-              className="h-9 w-auto object-contain sm:h-10 lg:h-11"
-            />
+  src="/logo-clean.png"
+  alt="U-MOVEX Orlando Movers"
+  className="h-12 sm:h-14 lg:h-16 xl:h-[72px] w-auto object-contain"
+/>
           </Link>
 
           <nav className="hidden items-center gap-5 lg:flex xl:gap-6">
